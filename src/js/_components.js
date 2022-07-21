6c7533ce5c809c99ae824de00c68d4e55e5e7670
Vazbro <zborzhevskiyvova@gmail.com>
2022-07-21 09:50:04 +0300

    added wor-1 page

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import './components/index-slider';
 import './components/circle-progress';
 import './components/accordion';
+import './components/portfolio';
--- src/js/components/index-slider.js
+++ src/js/components/index-slider.js
@@ -1,8 +1,8 @@
 import Swiper, {
-  Navigation
+  Navigation, Thumbs
 } from 'swiper';
 
-Swiper.use([Navigation]);
+Swiper.use([Navigation, Thumbs]);
 
 const portSlider = document.querySelector('.portfolio-section__items');
 const relatedSlider = document.querySelector('.related-projects__items');
@@ -141,3 +141,25 @@ const testimonialsSwiperSlider = new Swiper(testimonialsSlider, {
     prevEl: '.testimonials__prev',
   },
 });
+
+const workImages = document.querySelector('.work-images-slider');
+
+if (workImages) {
+  const workSlider = new Swiper('.work-images-nav', {
+    spaceBetween: 20,
+    slidesPerView: 10,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  const workSlidesNav = new Swiper(workImages, {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".work-images__next",
+      prevEl: ".work-images__prev",
+    },
+    thumbs: {
+      swiper: workSlider,
+    },
+  });
+};
